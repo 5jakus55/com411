ce12f0d200d40ee2ec1dc5ae7eb8ab499513cbd3
repl.it user <replituser@example.com>
2020-10-06 11:26:58 +0000

    added code to demonstrate how a variable can be used as a computer

---
+++ basics/decisions/simple_decision/counter.py
@@ -0,0 +1,10 @@
+print("Please enter the first whole number.")
+first_number = int(input())
+print("Please enter the second whole number.")
+second_number = int(input())
+print("Please enter the third whole number.")
+third_number = int(input())
+even = 
+if ( first_number % 2 == 0) :
+  print("There were {} even and {} odd numbers.".format(even,odd))
+ 
