072f686b2e4256b8f4f26f71ff53c586b1dae969
repl.it user <replituser@example.com>
2020-10-06 09:51:04 +0000

    added code to demonstrate the if...else statement

--- basics/decisions/simple_decision/if.py
+++ basics/decisions/simple_decision/if.py
@@ -1,5 +1,5 @@
 print("What type of book is this ? ")
 book = input()
-if (len(book) > 5) :
+if (book == "adventure") :
   print("I like {} books!".format(book))
 print("Finished reading book")
--- basics/decisions/simple_decision/if_else.py
+++ basics/decisions/simple_decision/if_else.py
@@ -1,7 +1,6 @@
 print("Please enter the activity to be performed ")
 activity = input()
-
-if  (activity == "calculate") :
+if (activity = "calculate") :
   print("Performing calculations")
-else : print("Performing activity")
+else print("Performing activity")
 print("Activity completed!")
