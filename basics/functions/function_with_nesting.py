fe180390e9fa619a8c2431fbae796e8e40e8b9cb
repl.it user <replituser@example.com>
2020-10-14 17:40:17 +0000

    function_parameter

---
+++ basics/functions/function_with_nesting.py
@@ -0,0 +1,8 @@
+def identify() :
+ print("\nWhat lies before us?")
+ response = input()
+ if (response.lower() == "a large boulder") :
+    print("It's time to run!")
+ else : print("We will be fine.")
+
+identify()
