f634465c18e8f2090d3e4d9c6388bcd1cf2bd6d5
repl.it user <replituser@example.com>
2020-10-22 12:05:43 +0000

    added code to demonstrate how to nest tuples and lists

---
+++ data/tuples/function_tuple.py
@@ -0,0 +1,10 @@
+def likelihood():
+  likelihoods = (50, 38, 27, 99, 4)
+  return min(likelihoods), max(likelihoods)
+
+def run():
+  values =likelihood()
+  print("Minimum likelihood of falling: {}".format(values[0]))
+  print("Maximum likelihood of falling: {}".format(values[1]))
+
+run()
