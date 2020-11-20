5f9797fe404d54486d160a645596e2c5bf7f3d89
repl.it user <replituser@example.com>
2020-11-20 12:42:13 +0000

    simple_subplots

--- visual/subplots/simple_sub.py
+++ visual/subplots/simple_sub.py
@@ -1,3 +1,20 @@
-def read_data(path_name):
-  with open("temps.txt") as file:
-    lines = file.read().split("\n")
+import matplotlib.pyplot as plt
+
+def read_data(file_path):
+  with open(file_path) as file:
+    temperatures = []
+    for line in file:
+      temperature = float(line.stip())
+      temperatures.append(temperature)
+
+  return temperatures
+
+def run():
+  data = read_data('visual/subplots/temps.txt')
+  fig, (ax1,ax2)= plt.subplots(1,2)
+  ax1.plot(range(1,8),data)
+  ax2.bar(range(1,8),data)
+  plt.tight_layout()
+  plt.show()
+
+run() 
